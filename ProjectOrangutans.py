071a907b1eda9ff8c5624e7550ef149999602b7f
BrianK66618 <kbrian0618@gmail.com>
2023-10-23 08:22:59 -0400

    Added Time and system library/variable for timeToSleep & Stable

--- ProjectOrangutans.py
+++ ProjectOrangutans.py
@@ -3,6 +3,12 @@ Our Welcome Screen will start our program letting
 drivers know that the Infotech Center 2023 is loading
 """
 
-print("\n\n Welcome - InfoTech Center 2023")
+#Import_Libraries_Here
+import sys
+import time
 
+timeToSleep = 2
+
+print("\n\n Welcome - InfoTech Center 2023")
+time.sleep(timeToSleep)
 print("\n InfoTech Center 2023 is loading...")
