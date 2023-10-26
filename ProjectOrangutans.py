3cd4b649a685a67a76c87de268da922da997b7e4
BrianK66618 <kbrian0618@gmail.com>
2023-10-26 08:26:06 -0400

    added a loading "while" loop
    Made it so there's loading text, and the dots from an ellipsis move. If it has an error, then it stops all of it and shuts it down. Every time it loads it stops the code for half a second for about 20 loops. The code often replaces the previous text in the file to make it so there's less lines for the users to read.

--- ProjectOrangutans.py
+++ ProjectOrangutans.py
@@ -3,12 +3,32 @@ Our Welcome Screen will start our program letting
 drivers know that the Infotech Center 2023 is loading
 """
 
-#Import_Libraries_Here
+# Import_Libraries_Here
 import sys
 import time
 
 timeToSleep = 2
 
-print("\n\n Welcome - InfoTech Center 2023")
+print("\n\n Welcome - InfoTech Center 2023\n")
 time.sleep(timeToSleep)
-print("\n InfoTech Center 2023 is loading...")
+
+# add code to have the ellipses add a dot every .5 seconds
+x = 0
+ellipsis = 1
+while x != 20:
+    x += 1
+    message = ("InfoTech Center 2023 is loading" + "." * ellipsis)
+    sys.stdout.write(
+        "\r" + message)  # /r prints a carriage return first, so, message is printed on top of previous line
+    time.sleep(0.5)
+    if ellipsis == 3:
+        ellipsis = 0
+    elif ellipsis <= 3:
+        ellipsis += 1
+    else:
+        sys.stdout.write("\r error")
+        sys.exit()
+    if x == 20:
+        print("InfoTech Center 2023 finished loading")
+        time.sleep(1)
+        print("\n Operating System Loaded - Face Scanned - Access Granted")
