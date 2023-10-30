864793b1febefdcc5ba900659adfd4a7d7561456
BrianK66618 <kbrian0618@gmail.com>
2023-10-30 08:16:59 -0400

    added gasLevelAlert function-milesToGasStation variable random/round

--- ProjectOrangutans.py
+++ ProjectOrangutans.py
@@ -6,8 +6,14 @@ import sys
 from time import sleep
 import random
 
+# function that lists Gas Stations and randomly choosing one, and returning its value
 def gasLevelGuage():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Thank", "Full Tank"]
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+# function will call the gasLevelGuage to determine gas level and then find a close gas station, if low.
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(3,30)12 )
+    print(milesToGasStationLow)
+gasLevelAlert()
